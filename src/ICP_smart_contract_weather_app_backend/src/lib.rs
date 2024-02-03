95f154fe70d27f824a88ad884f63e0a1547f4be5
“vitalvirtue” <hyterdem@hotmail.com>
2024-02-03 21:53:37 +0300

    external libraries and defines virtual memory

--- src/ICP_smart_contract_weather_app_backend/src/lib.rs
+++ src/ICP_smart_contract_weather_app_backend/src/lib.rs
@@ -1,4 +1,9 @@
-#[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
-}
+// External libraries used in Rust are defined here
+use candid::{CandidType, Decode, Deserialize, Encode}; // Enables the availability of external libraries
+use ic_stable_structures::memory_manager::{MemoryId, MemoryManager, VirtualMemory}; // Contains necessary structures for memory management
+use ic_stable_structures::{BoundedStorable, DefaultMemoryImpl, StableBTreeMap, Storable}; // Contains structures for storage operations
+use std::{borrow::Cow, cell::RefCell}; // Includes certain structures from the standard library
+
+// Defines the type of virtual memory
+type Memory = VirtualMemory<DefaultMemoryImpl>;
+
