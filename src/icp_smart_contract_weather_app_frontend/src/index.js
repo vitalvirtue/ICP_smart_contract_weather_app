5efbc0550c70ad36c09e47c29de63bb657ad4dc6
“vitalvirtue” <hyterdem@hotmail.com>
2024-02-05 03:02:56 +0300

    some changes on front end

--- src/icp_smart_contract_weather_app_frontend/src/index.js
+++ src/icp_smart_contract_weather_app_frontend/src/index.js
@@ -1,21 +1,10 @@
 import { icp_smart_contract_weather_app_backend } from "../../declarations/icp_smart_contract_weather_app_backend";
 
-async function getWeatherData() {
-    const button = document.querySelector("button");
-    button.setAttribute("disabled", true);
-
-    // Interact with Rust actor, calling the get_weather_data method
-    const data = await icp_smart_contract_weather_app_backend.update_weather_data();
-
-    button.removeAttribute("disabled");
-
-    // Alınan veriler HTML elementlerine eklenir.
-    const weatherDataDiv = document.getElementById('weatherData');
-    weatherDataDiv.innerHTML = `
-        <p>Temperature: ${data.temperature}</p>
-        <p>Pressure: ${data.pressure}</p>
-        <p>Humidity: ${data.humidity}</p>
-        <p>Wind Speed: ${data.wind_speed}</p>
-    `;
-}
-
+document.getElementById('getWeatherButton').addEventListener('click', async function() {
+    try {
+        const weatherData = await icp_smart_contract_weather_app_backend.get_weather_data();
+        document.getElementById('weatherData').innerText = JSON.stringify(weatherData);
+    } catch (error) {
+        console.error('Error:', error);
+    }
+});
